98be89190c21307fbe4424ae50bdfc836d412207
root <root@localhost.localdomain>
2016-09-09 14:53:34 -0700

    new

---
+++ tcp.py
@@ -0,0 +1,17 @@
+import socket
+
+
+target_host = 'www.google.com'
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((target_host,target_port))
+
+data = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n".encode(encoding='utf-8')
+
+client.send(data)
+
+response = client.recv(4096)
+
+print(response)
